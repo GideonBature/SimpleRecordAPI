9aeb15e600d41a02350cb4e94fa7d9a636e2c6dc
GideonBature <infoaboutgideon@gmail.com>
2024-05-08 18:29:59 +0100

    PUT and DELETE method added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,8 @@ const userController = require('../controllers/userController');
 
 router.post('/users', userController.createUser);
 router.get('/users', userController.getAllUsers);
+router.get('/users/:id', userController.getUser);
+router.put('/users/:id', userController.updateUser);
+router.delete('/users/:id', userController.deleteUser);
 
 module.exports = router;
