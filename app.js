d1fcfe9565ee40d3c428e5a7d235f3eae72d996d
GideonBature <infoaboutgideon@gmail.com>
2024-05-10 11:35:32 +0100

    make few changes

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 
-const port = 8080;
+require('dotenv').config();
+
+const port = process.env.PORT;
 
 const app = express();
 
 app.use(express.json());
 app.use('/api', userRoutes);
 
-mongoose.connect('mongodb://localhost:27017/userDB')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     app.listen(port, () => {
         console.log(`Server listening on port ${port}`);
