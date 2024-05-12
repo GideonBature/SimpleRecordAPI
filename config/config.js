9b6a313fb8d77177a0a135999c91dce36584fdbb
GideonBature <infoaboutgideon@gmail.com>
2024-05-12 14:11:37 +0100

    fix special character error in mongodb password

--- config/config.js
+++ config/config.js
@@ -3,9 +3,8 @@ const dotenv = require('dotenv');
 // loading environment variables
 dotenv.config();
 
-const port = process.env.PORT || 3000;
-const db =  process.env.MONGODB_URI || 'mongodb+srv://GideonBature:2014/1/52201Ea@userdb.v9xecup.mongodb.net/?retryWrites=true&w=majority&appName=userDB'
-;
-const secret = process.env.JWT_SECRET || '2014/1/52201Ea03121997GiDeOn7332BaTuRe';
+const port = process.env.PORT;
+const db =  process.env.MONGODB_URI;
+const secret = process.env.JWT_SECRET;
 
 module.exports = { port, db, secret };
